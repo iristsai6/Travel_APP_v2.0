7996b89c5390ec97b70402393accbc7f84aec17d
iristsai6 <iristsai6@gmail.com>
2026-07-03 03:36:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { renderItineraryPage } from "./itinerary.js";
 import { renderMapPage } from "./map.js";
 import { renderFinancePage } from "./finance.js";
 
+// 路由表配置
 const routes = {
     "#home": renderHomePage,
     "#itinerary": renderItineraryPage,
@@ -10,11 +11,18 @@ const routes = {
     "#finance": renderFinancePage
 };
 
+// 路由掛載引擎
 function router() {
     const hash = window.location.hash || "#home";
     const container = document.getElementById("app-root");
-    if (routes[hash]) routes[hash](container);
+    
+    // 檢查路由是否存在，若無則預設導向 Home
+    const routeFunc = routes[hash] || renderHomePage;
+    
+    // 執行對應頁面渲染
+    routeFunc(container);
 }
 
+// 監聽 URL 變更與頁面載入
 window.addEventListener("hashchange", router);
 window.addEventListener("load", router);
